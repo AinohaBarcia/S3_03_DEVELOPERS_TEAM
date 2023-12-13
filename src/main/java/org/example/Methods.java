271d2043df728fa58ab57ebb38344db99ddb6ecb
judithgispert <jugispert@gmail.com>
2023-12-13 10:17:33 +0100

    TODO done + change variable and equals

--- src/main/java/org/example/Methods.java
+++ src/main/java/org/example/Methods.java
@@ -6,23 +6,20 @@ import java.util.Locale;
 public class Methods {
 
     public static void createTree(List<Product> productsList){
-        //TODO change names
         productsList.add(new Tree(Input.getString("Tree name:"), Input.getFloat("Price:"), Input.getFloat("Height:")));
     }
     public static void createFlower(List<Product>productsList){
-        //TODO change names
         productsList.add(new Flower(Input.getString("Flower name:"), Input.getFloat("Price:"), Input.getString("Colour:")));
     }
     public static void createDecoration(List<Product>productsList){
-        //TODO change names
-        String letterMaterial = Input.getString("Decoration material: Wood or Plastic?").toUpperCase(Locale.ROOT).charAt(0);
-        if (letterMaterial.equals("P"){
+        char letterMaterial = Input.getString("Decoration material: Wood or Plastic?").toUpperCase().charAt(0);
+        if (letterMaterial == 'P'){
             Decoration.DecorationMaterial plastic = Decoration.DecorationMaterial.PLASTIC;
             productsList.add(new Decoration(Input.getString("Decoration name:"), Input.getFloat("Price:"), plastic));
-        } else if (letterMaterial.equals("W")){
+        }else if (letterMaterial == 'W'){
             Decoration.DecorationMaterial wood = Decoration.DecorationMaterial.WOOD;
             productsList.add(new Decoration(Input.getString("Decoration name:"), Input.getFloat("Price:"), wood));
-        } else{
+        }else{
             System.out.println("Wrong material.");
         }
     }
