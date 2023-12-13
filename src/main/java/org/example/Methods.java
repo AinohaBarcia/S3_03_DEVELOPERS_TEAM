00223ef84053e24927460cfc602339761394b060
judithgispert <jugispert@gmail.com>
2023-12-13 10:53:12 +0100

    New method + change names

--- src/main/java/org/example/Methods.java
+++ src/main/java/org/example/Methods.java
@@ -5,22 +5,26 @@ import java.util.Locale;
 
 public class Methods {
 
-    public static void createTree(List<Product> productsList){
-        productsList.add(new Tree(Input.getString("Tree name:"), Input.getFloat("Price:"), Input.getFloat("Height:")));
+    public static void createTree(List<Product> productList){
+        productList.add(new Tree(Input.getString("Tree name:"), Input.getFloat("Price:"), Input.getFloat("Height:")));
     }
-    public static void createFlower(List<Product>productsList){
-        productsList.add(new Flower(Input.getString("Flower name:"), Input.getFloat("Price:"), Input.getString("Colour:")));
+    public static void createFlower(List<Product>productList){
+        productList.add(new Flower(Input.getString("Flower name:"), Input.getFloat("Price:"), Input.getString("Colour:")));
     }
-    public static void createDecoration(List<Product>productsList){
+    public static void createDecoration(List<Product>productList){
         char letterMaterial = Input.getString("Decoration material: Wood or Plastic?").toUpperCase().charAt(0);
         if (letterMaterial == 'P'){
             Decoration.DecorationMaterial plastic = Decoration.DecorationMaterial.PLASTIC;
-            productsList.add(new Decoration(Input.getString("Decoration name:"), Input.getFloat("Price:"), plastic));
+            productList.add(new Decoration(Input.getString("Decoration name:"), Input.getFloat("Price:"), plastic));
         }else if (letterMaterial == 'W'){
             Decoration.DecorationMaterial wood = Decoration.DecorationMaterial.WOOD;
-            productsList.add(new Decoration(Input.getString("Decoration name:"), Input.getFloat("Price:"), wood));
+            productList.add(new Decoration(Input.getString("Decoration name:"), Input.getFloat("Price:"), wood));
         }else{
             System.out.println("Wrong material.");
         }
+    }
+
+    public static void showTicketList(List<Ticket>ticketList){
+        ticketList.forEach(System.out::println);
     }
 }
