7b9f248ebc5416666e489f822b45d9133d5c9f5f
judithgispert <jugispert@gmail.com>
2023-12-14 13:01:34 +0100

    One instance of flower shop in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        FlowerShop flowerShop = new FlowerShop("AJJ Flower Shop");
+        FlowerShop flowerShop = FlowerShop.getInstance("AJJ Flower Shop");
 
         flowerShop.getProductList().add(new Decoration("Green tree", 12.35f, Decoration.DecorationMaterial.PLASTIC));
 
